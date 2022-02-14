b2d1e33126b1cd0a22263494df1f019dc97da6e9
Nicolas <nicolashereigo@gmail.com>
2022-02-14 12:09:10 +0300

    Fixed bug when the db pointer was nil

--- model/notes/establish_connection.go
+++ model/notes/establish_connection.go
@@ -7,19 +7,21 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
-func (env *DBEnv) establishConnection(db *gorm.DB) func() {
-
-	// hacky way to extract db connection code into a separate function
-	// to avoid code duplication
+func (env *DBEnv) connectToDB() (*gorm.DB, func()) {
 
 	db, err := gorm.Open(env.DBDialect, env.DBName)
 	if err != nil {
 		fmt.Println(err.Error())
 		panic("Could not connect to the database")
 	}
+	fmt.Println("Connected to db")
 
-	return func() {
-		db.Close()
+	return db, func() {
+		err := db.Close()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Db connection ws closed")
 	}
 
 }
