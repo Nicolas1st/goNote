3fbdbe5590ff6f8e89894489bb39b2e4d47a5888
Nicolas <nicolashereigo@gmail.com>
2022-02-14 21:28:57 +0300

    Fixed styling in middleware.go

--- api/note/middlewares.go
+++ api/note/middlewares.go
@@ -1,8 +1,6 @@
 package api
 
-import (
-	"net/http"
-)
+import "net/http"
 
 func HeaderMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
