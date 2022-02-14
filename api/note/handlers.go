0fda5d85396f6dbf8ad92fa09680609e70f3af7a
Nicolas <nicolashereigo@gmail.com>
2022-02-14 21:31:12 +0300

    Updated note api handlers to use new db functions
    The handlers are now built dynamically

--- api/note/handlers.go
+++ api/note/handlers.go
@@ -1,23 +1,85 @@
 package api
 
-import "net/http"
+import (
+	"encoding/json"
+	"math/bits"
+	"net/http"
+	"strconv"
 
-func GetNote(w http.ResponseWriter, r *http.Request) {
+	"github.com/Nicolas1st/goNote/model/entities/note"
+	"github.com/gorilla/mux"
+)
+
+func BuildGet(f func(id uint) *note.Note) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		id, err := strconv.ParseUint(vars["id"], 10, bits.UintSize)
+		if err != nil {
+			panic(err)
+		}
+
+		note := *f(uint(id))
+
+		json.NewEncoder(w).Encode(note)
+	})
 
 }
 
-func GetAllNotes(w http.ResponseWriter, r *http.Request) {
+func BuildGetAll(f func(author string) *[]note.Note) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		author := vars["author"]
+		notes := f(author)
 
+		json.NewEncoder(w).Encode(notes)
+	})
 }
 
-func CreateNote(w http.ResponseWriter, r *http.Request) {
+func BuildCreate(f func(*note.Note) (*note.Note, error)) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		note := &note.Note{}
+		json.NewDecoder(r.Body).Decode(note)
 
+		note, err := f(note)
+		if err != nil {
+			panic(err)
+		}
+
+		json.NewEncoder(w).Encode(note)
+	})
 }
 
-func UpdateNote(w http.ResponseWriter, r *http.Request) {
+func BuildUpdate(f func(id uint, title, content string) *note.Note) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		id, err := strconv.ParseUint(vars["id"], 10, bits.UintSize)
+		if err != nil {
+			panic(err)
+		}
 
+		var noteWithFieldsToUpdate note.Note
+		err = json.NewDecoder(r.Body).Decode(&noteWithFieldsToUpdate)
+		if err != nil {
+			panic(err)
+		}
+
+		note := f(uint(id), noteWithFieldsToUpdate.Title, noteWithFieldsToUpdate.Content)
+
+		json.NewEncoder(w).Encode(note)
+	})
 }
 
-func DeleteNote(w http.ResponseWriter, r *http.Request) {
+func BuildDelete(f func(id uint) *note.Note) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		id, err := strconv.ParseUint(vars["id"], 10, bits.UintSize)
+		if err != nil {
+			panic(err)
+		}
 
+		note := f(uint(id))
+
+		json.NewEncoder(w).Encode(note)
+	})
 }
