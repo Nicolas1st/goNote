b0324ef9a317870c8c6a26f62d494eaaa044d126
Nicolas <nicolashereigo@gmail.com>
2022-02-14 21:37:47 +0300

    Updated main.go to use new model and api code

--- main.go
+++ main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+
+	api "github.com/Nicolas1st/goNote/api/note"
+	"github.com/Nicolas1st/goNote/model/database"
+	entity "github.com/Nicolas1st/goNote/model/entities/note"
+	"github.com/Nicolas1st/goNote/model/migrations"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
 func main() {
+
+	// db initialization
+	dbEnv := database.Env{
+		DatabaseDialect: "sqlite3",
+		DatabaseName:    "test.db",
+	}
+
+	// creating tables in the db if the don't exist
+	migrations.Initialize(&dbEnv)
+
+	// creating a router for notes resource
+	notesResource := api.NewNotesResourceRouter(entity.NoteEntity{
+		DatabaseEnv: &dbEnv,
+	})
+
+	// running the server
+	port := ":8880"
+	fmt.Printf("Server is listening on port %v\n", port)
+	http.ListenAndServe(port, notesResource)
 
 }
