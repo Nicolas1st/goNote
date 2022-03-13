40c0bf1c7d00a3d389794583629c3a89ee1dbf87
Nicolas <nicolashereigo@gmail.com>
2022-03-13 18:02:47 +0300

    Added code responsible for presentation

---
+++ static/js/presentation.js
@@ -0,0 +1,33 @@
+// accepts an array of note objects
+// can ether add new notes to the list,
+// or replace the old one with the new ones
+// depending on the params
+export function displayNoteComponents(replace, ...notes) {
+    const notesContainer = document.querySelector(".notes-container");
+
+    if (replace) {
+        notesContainer.innerHTML = "";
+    }
+
+    // adding notes
+    notes.forEach((note) => {
+        notesContainer.appendChild(newNoteComponent(note.Title, note.Content, note.ID));
+    });
+}
+
+export function removeNoteComponent(noteID) {
+    const notesContainer = document.querySelector(".notes-container");
+
+    const elementToBeRemoved = notesContainer.querySelector($`[data-id="${noteID}"]`)
+
+    notesContainer.removeChild(elementToBeRemoved);
+}
+
+export function updateNoteComponent(note){
+    const notesContainer = document.querySelector(".notes-container")
+
+    const newNote = newNoteComponent(note.Title, note.Content, note.ID);
+    const oldNote = notesContainer.querySelector($`[data-id="${note.ID}"]`)
+
+    notesContainer.replaceChild(newNote, oldNote);
+}
